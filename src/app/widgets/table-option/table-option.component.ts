f5c7fb0f9c7205380f48e58f1c40f5f338f35e4b
Jingjing Li <lijingjing1008@gmail.com>
2019-04-12 14:01:09 +0800

    Use dragondrag to enable accessibility.

--- src/app/widgets/table-option/table-option.component.ts
+++ src/app/widgets/table-option/table-option.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild, ElementRef } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import DragonDrop from 'drag-on-drop';
 
 @Component({
   templateUrl: './table-option.component.html',
   styleUrls: ['./table-option.component.scss']
 })
 export class TableOptionComponent implements OnInit {
+  @ViewChild('optionsEle')
+  optionsEle: ElementRef;
+  @ViewChild('selectedEle')
+  selectedEle: ElementRef;
+
   public options: any[];
   public selected: any[];
+  public dragonOptions;
+  public dragonSeletced;
 
   constructor(
     public dialogRef: MatDialogRef<TableOptionComponent>,
@@ -22,12 +30,92 @@ export class TableOptionComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.dragonOptions = new DragonDrop(this.optionsEle.nativeElement,
+      {
+        handle: false,
+        item: ':scope .name',
+        announcement: {
+          grabbed: (el) => {
+            // console.log(`${el.querySelector('.displayName').innerText} grabbed`);
+            return `${el.querySelector('.displayName').innerText} grabbed`;
+          },
+          dropped: (el, items) => {
+            // console.log(`${el.querySelector('.displayName').innerText} dropped`);
+            return `${el.querySelector('.displayName').innerText} dropped`;
+          },
+          reorder: (el, items) => {
+            const pos = items.indexOf(el) + 1;
+            const text = el.querySelector('.displayName').innerText;
+            // console.log(`The options columns order has changed, ${text} is now item ${pos} of ${items.length}`);
+            return `The options columns order has changed, ${text} is now item ${pos} of ${items.length}`;
+          },
+          cancel: 'Reschedule cancelled.'
+        }
+      });
+    this.dragonSeletced = new DragonDrop(this.selectedEle.nativeElement,
+      {
+        handle: false,
+        item: ':scope .name',
+        announcement: {
+          grabbed: (el, items) => {
+            return `${el.innerText} grabbed`;
+          },
+          dropped: el => {
+            return `${el.innerText} dropped`;
+          },
+          reorder: (el, items) => {
+            const pos = items.indexOf(el) + 1;
+            const text = el.innerText;
+            return `The selected columns order has changed, ${text} is now item ${pos} of ${items.length}`;
+          },
+          cancel: 'Reschedule cancelled.'
+        }
+      });
+
+  }
+
+  ngAfterViewInit() {
+    this.dragonSeletced.initElements(this.selectedEle.nativeElement);
+    this.dragonOptions.initElements(this.optionsEle.nativeElement);
+
   }
 
   get result() {
-    this.options.forEach(e => (e as any).displayed = false);
-    this.selected.forEach(e => (e as any).displayed = true);
-    return { columns: this.selected.concat(this.options) };
+    let optionsEles = Array.prototype.slice.apply(document.querySelectorAll('.option-item .displayName'));
+    let tempOptions = [];
+    optionsEles.forEach(element => {
+      let index = this.options.findIndex(ele => {
+        return element.innerText == ele.displayName;
+      });
+      tempOptions.push(this.options[index]);
+    });
+    let selectedEles = Array.prototype.slice.apply(document.querySelectorAll('.selected-item .displayName'));
+    let tempSelected = [];
+    selectedEles.forEach(element => {
+      let index = this.selected.findIndex(ele => {
+        return element.innerText == ele.displayName;
+      });
+      tempSelected.push(this.selected[index]);
+    });
+    // tempOptions.forEach(e => (e as any).displayed = false);
+    // tempSelected.forEach(e => (e as any).displayed = true);
+    return { columns: tempSelected.concat(tempOptions) };
+  }
+
+  addColumn($event, item) {
+    $event.stopPropagation();
+    let index = this.options.indexOf(item);
+    item.displayed = true;
+    this.selected.push(item);
+    this.options.splice(index, 1);
+  }
+
+  removeColumn($event, item) {
+    $event.stopPropagation();
+    let index = this.selected.indexOf(item);
+    item.displayed = false;
+    this.options.push(item);
+    this.selected.splice(index, 1);
   }
 
   close(res) {
